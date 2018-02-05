4d4bbfb6f93dd321ffafcab26ec7602d908963c8
Tycho <tycho1993@hotmail.com>
2018-02-05 13:32:55 +0100

    added good files

---
+++ TestGrid/TestGrid/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace TestGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1204, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1088, 525);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1088, 525);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.DoubleClick += new System.EventHandler(this.pictureBox2_DoubleClick);
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1088, 525);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.Move += new System.EventHandler(this.Form1_Move);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ TestGrid/TestGrid/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestGrid
+{
+
+    struct Coord : IEquatable<Coord>
+    {
+        public int x;
+        public int y;
+        public bool IsRed;
+
+        public bool Equals(Coord other)
+        {
+            return (x == other.x && y == other.y);
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private const int RectSize = 50;
+        private int PieceSize = RectSize- 10;
+        private bool Turn = true;
+        private List<Coord> Pieces = new List<Coord>();
+
+        private Graphics g;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.Controls.Add(pictureBox2);
+            pictureBox2.Location = new Point(0, 0);
+            pictureBox2.BackColor = Color.Transparent;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+ 
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+           base.OnPaint(e);
+
+            int x = 60;
+            int y = 60;
+
+            using (Pen Pen = new Pen(Color.Black))
+            {
+                for (int row = 0; row < 19; ++row)
+                {
+                    for (int col = 0; col < 19; ++col)
+                    {
+                        e.Graphics.DrawRectangle(Pen, x + (row * RectSize), y + (col * RectSize), RectSize, RectSize);
+                    }
+                }
+            }
+
+            foreach (Coord p in Pieces)
+            {
+                // if p.isred then red
+                // else black
+                DrawCircle(e, x, y, PieceSize, PieceSize);
+            }
+        }
+
+        private bool getPiecePosition(ref int x, ref int y)
+        {
+            if ((x - (60 + RectSize)) % RectSize <= 40 && 
+                (y - (60 + RectSize)) % RectSize <= 40)
+            {
+                x = ((x - (60)) / RectSize) * RectSize + 60;
+                y = ((y - (60)) / RectSize) * RectSize + 60;
+                return true;
+            }
+            return false;
+        }
+
+
+
+
+        private void  Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            int x = e.X;
+            int y = e.Y;
+
+            if (!getPiecePosition(ref x,ref y))
+            {
+                return;
+            }
+  
+            Rectangle rect = new Rectangle();
+            PaintEventArgs arg = new PaintEventArgs(g, rect);
+
+            //if (x > 20 * RectSize || y > 20 * RectSize || x < 2 * RectSize || y < 2 * RectSize)
+            //{
+            //    return;
+            //}
+            DrawCircle(arg, x, y, PieceSize, PieceSize);
+
+           // pictureBox1.Refresh();
+        }
+
+        private void DrawCircle(PaintEventArgs e, int x, int y, int width, int height)
+        {
+            Coord c;
+            c.x = x;
+            c.y = y;
+            c.IsRed = Turn;
+
+            var existsP = Pieces.Contains(c);
+
+            if (existsP)
+                return;
+
+            Pieces.Add(c);
+            Pen pen;
+
+            if (Turn)
+            {
+                pen = new Pen(Color.Red, 3);
+            } else 
+            {
+                pen = new Pen(Color.Black, 3);
+            }
+
+                 e.Graphics.DrawEllipse(pen, x - width / 2, y - height / 2, width, height);
+           Turn = !Turn;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void pictureBox2_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            pictureBox2.Visible = !pictureBox2.Visible;
+        }
+    }
+}
