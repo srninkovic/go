9fd76a0e2c90fdaf9eb7b4f0a0232bceecef3ca6
Ludo Stroetenga <ludo@stroetenga.nl>
2018-02-19 16:38:46 +0100

    ludo

---
+++ TestGrid/TestGrid/Board.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGrid
+{
+    internal class Board
+    {
+        internal void PlacePiece(bool turn, Piece p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
